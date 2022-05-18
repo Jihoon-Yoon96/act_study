bd607a0a414bb1be0c97102e6c8183ce8454453e
Jihoon-Yoon96 <85949211+Jihoon-Yoon96@users.noreply.github.com>
2022-05-18 16:34:31 +0900

    test commit by Brian

--- layouts/default.vue
+++ layouts/default.vue
@@ -106,6 +106,11 @@ export default {
           icon: 'mdi-chart-bubble',
           title: 'Inspire',
           to: '/inspire'
+        },
+        {
+          icon: 'mdi-table',
+          title: 'Test Space',
+          to: '/test'
         }
       ],
       miniVariant: false,
---
+++ pages/test.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-container>
+    <v-card>
+      <v-card-title>Hello World</v-card-title>
+      <v-card-text>
+        It's ACT DEV1 & DEV2 Junior DEV Study Workspace
+      </v-card-text>
+    </v-card>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "test"
+}
+</script>
+
+<style scoped>
+
+</style>
