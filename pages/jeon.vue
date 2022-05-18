9a2f67bb6a8ae9ca81f9e458589aaa5f4f059c9b
jeontaedoo <ta3794@naver.com>
2022-05-19 00:26:38 +0900

    jeon test

---
+++ pages/jeon.vue
@@ -0,0 +1,36 @@
+<template>
+    <div id="app">
+        <h2>hello {{userName}}</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ActStudyJeon',
+
+    data() {
+        return {
+            
+        };
+    },
+
+    mounted() {
+        
+    },
+
+    methods: {
+        
+    },
+};
+
+const app = new Vue({
+    el : "#app",
+    data : {
+        userName : "jeon"
+    }
+})
+</script>
+
+<style lang="scss" scoped>
+
+</style>
