927d7562a8c6bc7e791d4da82aac27643aab9839
jeontaedoo <ta3794@naver.com>
2022-06-30 00:47:05 +0900

    .

---
+++ components/speed_deliver.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-container>
+  <h3>배송 정보</h3>
+  <v-card>
+    <h3>빠른배송 5,000원</h3>
+    <p>지금 결제시</p>
+  </v-card>
+  <v-card>
+    <h3>일반배송 3,000원</h3>
+    <p>검수 후 배송 ・ 5-7일 내 도착 예정</p>
+  </v-card>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "speed_deliver"
+}
+</script>
+
